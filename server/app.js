aa35a9d49fb4590c09617ebc2f937b290661fce0
fazilsnippet <ahfazilahfazil@gmail.com>
2026-04-01 18:50:05 +0530

    feature:frotended

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://car-mart-client.onrender.com", 
     credentials: true,
   })
 );
--- server/services/chat.socket.js
+++ server/services/chat.socket.js
@@ -9,7 +9,7 @@ const onlineUsers = new Map();
 export const initSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://car-mart-client.onrender.com",
       credentials: true,
     },
   });
