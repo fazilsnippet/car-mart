efce5dadb2c0acc961b3c4f30b914e9dfae2bfc6
fazilsnippet <ahfazilahfazil@gmail.com>
2026-05-18 22:59:39 +0530

    feature:neck-breaker

--- server/middleware/parseRequest.middleware.js
+++ server/middleware/parseRequest.middleware.js
@@ -1,13 +1,39 @@
+// export const parseRequestData = (req, res, next) => {
+//   try {
+//     if (req.body.data) {
+//       req.body = JSON.parse(req.body.data);
+//     }
+//     next();
+//   } catch {
+//     return res.status(400).json({
+//       success: false,
+//       message: "Invalid JSON payload"
+//     });
+//   }
+// };
+
 export const parseRequestData = (req, res, next) => {
   try {
-    if (req.body.data) {
-      req.body = JSON.parse(req.body.data);
+    if (req.body.location && typeof req.body.location === "string") {
+      req.body.location = JSON.parse(req.body.location);
     }
+
+    if (req.body.features) {
+      if (typeof req.body.features === "string") {
+        req.body.features = JSON.parse(req.body.features);
+      }
+
+      // handle features[]
+      if (!Array.isArray(req.body.features)) {
+        req.body.features = [req.body.features];
+      }
+    }
+
     next();
-  } catch {
+  } catch (error) {
     return res.status(400).json({
       success: false,
-      message: "Invalid JSON payload"
+      message: "Invalid multipart JSON fields",
     });
   }
 };
