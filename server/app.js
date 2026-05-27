dd9c00fa3c6baebc66c580f8f9824f04483e4a0b
fazilsnippet <ahfazilahfazil@gmail.com>
2026-05-27 14:59:28 +0530

    feature:neck-breaker

--- server/app.js
+++ server/app.js
@@ -30,8 +30,8 @@ app.use(express.json());
 app.use(
   cors({
     origin:
-    "http://localhost:5173",
-    // "https://car-mart-client.onrender.com",
+    // "http://localhost:5173",
+    "https://car-mart-client.onrender.com",
   credentials: true,
   })
 );
